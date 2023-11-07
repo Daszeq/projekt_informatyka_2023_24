9e1421c2818cb1142e7d0112480c9183e86c9793
Daszeq <jak.daszek@gmail.com>
2023-11-07 21:30:21 +0100

    wstęp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*tytu³: "project bee"
+aktualny zamys³ jest na zrobienie gry z unikaniem przeszkód i zdobywaniem punktów
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "Hellow World!" << std::endl;
+	return 0;
+}
